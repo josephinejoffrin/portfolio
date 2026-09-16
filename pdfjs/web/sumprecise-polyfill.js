6df54ead45a03a537a02dbc5a716b6a782b40728
josephinejoffrin <josephine.joffrin9@gmail.com>
2026-09-16 23:25:40 +0200

    Test PDF.js legacy 6.3.289

--- pdfjs/web/sumprecise-polyfill.js
+++
@@ -1,9 +0,0 @@
-if (typeof Math.sumPrecise !== 'function') {
-  Math.sumPrecise = function (iterable) {
-    let sum = 0;
-    for (const n of iterable) {
-      sum += n;
-    }
-    return sum;
-  };
-}
